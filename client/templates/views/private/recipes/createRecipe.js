ab0ceba15d74fbc8cb6ef215c512cff0ca891e6f
abayta <abayta10@gmail.com>
2016-05-04 08:42:31 +0200

    Añadir ingredientes a la receta sin persistencia en la base de datos

--- client/templates/views/private/recipes/createRecipe.js
+++ client/templates/views/private/recipes/createRecipe.js
@@ -19,9 +19,24 @@ Template.searchResult.rendered = function(){
   IngredientsSearch.search('');
 };
 
+Template.searchResult.events({
+  "click .add-ingredient": function (event, template) {
+      console.log(this);
+      document.getElementsByClassName('autoform-add-item')[0].click();
+      var lista = document.getElementsByClassName('list-group')[0].getElementsByTagName('li');
+      var last = lista[lista.length-2];
+      var inputN = last.getElementsByClassName('form-control');
+      inputN[0].value = this._id;
+      inputN[1].value = this.name;
+      console.log(last);
+
+  }
+});
+
 Template.searchBox.events({
   "keyup #search-box": _.throttle(function(e) {
     let text = $(e.target).val().trim();
     IngredientsSearch.search(text);
   }, 200)
 });
+
