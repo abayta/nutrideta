18196702b49f8983973e92283ed251663bf120c6
danjimgar <dani_el_del_viso@hotmail.com>
2016-06-22 17:25:53 +0200

    Ver mensaje

--- client/templates/views/private/messages/viewMessage.js
+++ client/templates/views/private/messages/viewMessage.js
@@ -1,3 +1,9 @@
 /**
  * Created by Casa on 12/05/2016.
  */
+
+Template.viewMessage.helpers({
+    message: function () {
+        return Messages.findOne({_id: Session.get("activeMessages")});
+    },
+});
