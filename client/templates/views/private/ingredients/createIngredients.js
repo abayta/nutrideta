4711fb08584975ad81cc57cc0ae1247d219cbf20
abayta <abayta10@gmail.com>
2016-07-27 14:36:54 +0200

    Trabajo

--- client/templates/views/private/ingredients/createIngredients.js
+++ client/templates/views/private/ingredients/createIngredients.js
@@ -1,3 +1,15 @@
+AutoForm.addHooks('insertIngredientsForm', {
+  onSuccess: function(){
+    console.log("El hook ha funcionado")
+    swal({
+        title: "¡Correcto!",
+        text: "Un ingrediente se ha añadido",
+        timer: 2000,
+        type: "success"
+    });
+  }
+});
+
 Template.listIngredients.onCreated(function() {
   this.autorun(() => {
     this.subscribe('ingredients');
