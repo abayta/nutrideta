763010ef0753a7d4f5c4ffc97ada37e619897000
danjimgar <dani_el_del_viso@hotmail.com>
2016-08-16 17:06:39 +0200

    Tests

--- tests/gagarin/test.js
+++ tests/gagarin/test.js
@@ -5,6 +5,6 @@
 describe('My first Gagarin test suite', function () {
     var server = meteor();
     it('should just work', function () {
-        return server.execute(function () { console.log('I am alive!'); });
+        return server.execute(function () { console.log('I am alive!');});
     });
 });
