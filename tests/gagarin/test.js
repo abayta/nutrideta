385c4572a2d25d7d8b59315e0e56c503e4fd8a3e
danjimgar <dani_el_del_viso@hotmail.com>
2016-08-16 17:05:11 +0200

    Tests

---
+++ tests/gagarin/test.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Casa on 16/08/2016.
+ */
+
+describe('My first Gagarin test suite', function () {
+    var server = meteor();
+    it('should just work', function () {
+        return server.execute(function () { console.log('I am alive!'); });
+    });
+});
